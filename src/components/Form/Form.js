6d22917c4feb69ca84a0c0fcaa652ae3f8982d0a
gustavomsmdt <gustavoschmidt147@gmail.com>
2023-03-21 21:19:34 -0300

    feat: add logic to get inputs value and add hook `useState()`

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,8 +2,10 @@ import './Form.css';
 import TextInput from '../TextInput'
 import SelectInput from '../SelectInput';
 import Button from '../Button';
+import { useState } from 'react';
 
 export function Form() {
+
     const teams = [
         'Loud',
         'Furia',
@@ -12,21 +14,49 @@ export function Form() {
         'NIP',
         'Sentinels',
         'Team Liquid'
-    ]
+    ];
+
+    const [name, setName] = useState('');
+    const [agent, setAgent] = useState('');
+    const [image, setImage] = useState('');
+    const [team, setTeam] = useState('');
 
     function submit(event) {
         event.preventDefault();
-        console.log('Vou submeter o formulário');
+        console.log(`Vou submeter o formulário: ${name}, ${agent}, ${image}, ${team}`);
     }
 
     return (
         <section className='form'>
             <form onSubmit={submit}>
                 <h2>Preencha os dados para criar o card do player</h2>
-                <TextInput mandatory={true} label='Nome' placeholder='Digite seu nome' />
-                <TextInput mandatory={true} label='Agente' placeholder='Digite sua classe de agente' />
-                <TextInput label='Imagem' placeholder='Digite o endereço da imagem' />
-                <SelectInput label='Organização' itens={teams} />
+                <TextInput
+                    mandatory={true}
+                    label='Nome'
+                    placeholder='Digite seu nome'
+                    value={name}
+                    whenEdited={value => setName(value)}
+                />
+                <TextInput
+                    mandatory={true}
+                    label='Agente'
+                    placeholder='Digite sua classe de agente'
+                    value={agent}
+                    whenEdited={value => setAgent(value)}
+                />
+                <TextInput
+                    label='Imagem'
+                    placeholder='Digite o endereço da imagem'
+                    value={image}
+                    whenEdited={value => setImage(value)}
+                />
+                <SelectInput
+                    mandatory={true}
+                    label='Organização'
+                    itens={teams} 
+                    value={team}
+                    whenEdited={value => setTeam(value)}
+                />
                 <Button>
                     Criar Card
                 </Button>
