5921d04c59d97c50bbd16ea52aae773f9363e433
gustavomsmdt <gustavoschmidt147@gmail.com>
2023-03-21 21:17:37 -0300

    feat: create logic to get text input value

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,10 +1,22 @@
 import './TextInput.css'
 
 export function TextInput (props) {
+
+    const whenTyped = (event) => {
+        props.whenEdited(event.target.value);
+    }
+
     return (
         <div className='text-input'>
-            <label>{props.label}</label>
-            <input required={props.mandatory} placeholder={props.placeholder}/>
+            <label>
+                {props.label}
+            </label>
+            <input
+                value={props.value}
+                onChange={whenTyped}
+                required={props.mandatory}
+                placeholder={props.placeholder}
+            />
         </div>
     );
 }
