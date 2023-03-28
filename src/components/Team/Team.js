59b6d3fc4bafaaf98241230f85853d6404c73011
gustavomsmdt <gustavoschmidt147@gmail.com>
2023-03-28 20:56:22 -0300

    feat: create team component

---
+++ src/components/Team/Team.js
@@ -0,0 +1,24 @@
+import Collaborator from '../Collaborator';
+import './Team.css';
+
+export function Team(props) {
+
+    const colorSecondary = { background: props.colorSecondary };
+    const colorPrimary = { border: props.colorPrimary };
+    
+    return (
+        props.collaborators.length > 0 && <section className='team' style={colorSecondary}>
+            <h3 style={colorPrimary}>
+                {props.name}
+            </h3>
+            <div className='collaborators'>
+                {props.collaborators.map(collaborator => <Collaborator 
+                    name={collaborator.name} 
+                    agent={collaborator.agent}
+                    image={collaborator.image}
+                />)}
+            </div>
+        </section>
+    )
+
+}
