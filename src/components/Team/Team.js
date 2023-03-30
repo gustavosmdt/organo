b79ce17ab53efec1aa6bae23d529802933f3002e
Gustavo Schmidt <gustavoschmidt147@gmail.com>
2023-03-30 09:08:30 -0300

    feat: add header brackground-color and attr key to Team component

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -7,12 +7,15 @@ export function Team(props) {
     const colorPrimary = { border: props.colorPrimary };
     
     return (
-        props.collaborators.length > 0 && <section className='team' style={colorSecondary}>
+        props.collaborators.length > 0 && 
+        <section className='team' style={colorSecondary}>
             <h3 style={colorPrimary}>
                 {props.name}
             </h3>
             <div className='collaborators'>
                 {props.collaborators.map(collaborator => <Collaborator 
+                    color={props.colorPrimary}
+                    key={collaborator.name}
                     name={collaborator.name} 
                     agent={collaborator.agent}
                     image={collaborator.image}
