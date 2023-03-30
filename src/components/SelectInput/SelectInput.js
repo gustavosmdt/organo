2f75a54fd5d44729a1b62b53e629f048b255d6e1
Gustavo Schmidt <gustavoschmidt147@gmail.com>
2023-03-30 09:05:54 -0300

    feat: add attr value to select

--- src/components/SelectInput/SelectInput.js
+++ src/components/SelectInput/SelectInput.js
@@ -1,12 +1,15 @@
 import './SelectInput.css'
 
 export function SelectInput(props) {
+    
     return (
         <div className='select-input'>
             <label>{props.label}</label>
-            <select onChange={event => props.whenEdited(event.target.value)}
-                    required={props.mandatory}>
-                <option key="Selecione">Selecione</option>
+            <select
+                onChange={event => props.whenEdited(event.target.value)}
+                required={props.mandatory}
+                value={props.value}>
+                <option key="Selecione" value="Selecione">Selecione</option>
                 {props.itens.map(item => {
                     return <option key={item}>{item}</option>
                 })}
